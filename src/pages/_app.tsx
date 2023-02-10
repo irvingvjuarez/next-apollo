a67b4fa2af08ba97f6eca17f90c657cd0e0245d8
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-09 20:49:27 -0600

    getStaticProps done in the home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
-
-const client = new ApolloClient({
-  uri: "https://countries.trevorblades.com/",
-  cache: new InMemoryCache()
-})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <Component {...pageProps} />
   )
 }
---
+++ src/type.d.ts
@@ -0,0 +1,7 @@
+type Country = {
+    name: string;
+    capital: string;
+    currency: string;
+    code: string;
+    emoji: string;
+}
