b76c405515654111d10af7d1d1927dc8d2009794
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-11 18:21:15 -0600

    static site generation applied to dynamic routing with apollo

---
+++ src/pages/countries/[id].tsx
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client"
+import { GetStaticProps, InferGetStaticPropsType } from "next"
+import { useRouter } from "next/router"
+import { client } from "@/services/client.apollo"
+
+export const getStaticPaths = async () => {
+	const { data } = await client.query({
+    query: gql`
+      query {
+        countries {
+          name
+          code
+          capital
+          currency
+          emoji
+        }
+      }
+    `
+  })
+
+	const countries = (data.countries as Country[]).slice(0, 10).map(country => ({
+		params: {
+			id: country.code
+		}
+	}))
+
+	return {
+		paths: countries,
+		fallback: true
+	}
+}
+
+export const getStaticProps: GetStaticProps<{ country: Country }> = async ({ params }) => {
+	const code = params?.id
+
+	const { data } = await client.query({
+		query: gql`
+			query Country ($code: ID!){
+				country(code: $code) {
+					name
+					capital
+					currency
+					code
+				}
+			}
+		`,
+		variables: { code }
+	})
+
+	return {
+		props: { country: data.country }
+	}
+}
+
+const CountryDetail: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ country }) => {
+	return (
+		<h2>Country: {country?.name}</h2>
+	)
+}
+
+export default CountryDetail
